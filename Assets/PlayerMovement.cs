558cbaba1749bed91c5e270b5fcc14bb088b21c7
LAPTOP-LHT7QQQG\zuko5 <tev218@nyu.edu>
2018-10-14 23:19:43 -0400

    Everything

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+	public GameObject trailPrefab;
+	public float counter;
+	public float Speed = 0.5f;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void Update()
+	{
+		counter += Time.deltaTime;
+		if (counter >= 3.0f)
+		{
+			
+		}
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate()
+	{
+		//float moveHorizontal = Input.GetAxis("Horizontal");
+
+		if (Input.GetKey(KeyCode.UpArrow))
+		{
+			transform.Translate(Vector2.up * Speed * Time.deltaTime);
+			
+		}
+
+		if (Input.GetKey(KeyCode.DownArrow))
+		{
+			transform.Translate(Vector2.down * Speed * Time.deltaTime);
+		}
+		
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.Translate(Vector2.right * Speed * Time.deltaTime);
+		}
+		
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.Translate(Vector2.left * Speed * Time.deltaTime);
+		}
+	}
+}
---
+++ Assets/PlayerTwoMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTwoMovement : MonoBehaviour {
+
+	public float Speed = 0.5f;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetKey(KeyCode.W))
+		{
+			transform.Translate(Vector2.up * Speed * Time.deltaTime);
+		}
+
+		if (Input.GetKey(KeyCode.S))
+		{
+			transform.Translate(Vector2.down * Speed * Time.deltaTime);
+		}
+		
+		if (Input.GetKey(KeyCode.D))
+		{
+			transform.Translate(Vector2.right * Speed * Time.deltaTime);
+		}
+
+		if (Input.GetKey(KeyCode.A))
+		{
+			transform.Translate(Vector2.left * Speed * Time.deltaTime);
+		}
+	}
+}
